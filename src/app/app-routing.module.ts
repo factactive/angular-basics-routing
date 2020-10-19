cc7a58aa5cf0942a0dca676ce4b24a7ce921423d
factactive <60749898+factactive@users.noreply.github.com>
2020-10-19 16:28:51 -0400

    added exception handling for unknown pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,18 @@ import {HomeComponent} from './home/home.component';
 import {AboutComponent} from './about/about.component';
 import {PostsComponent} from './posts/posts.component';
 import {PostComponent} from './post/post.component';
+import {AboutExtraComponent} from './about-extra/about-extra.component';
+import {ErrorPageComponent} from './error-page/error-page.component';
 
 const routes: Routes = [
     {path: '', component: HomeComponent},
-    {path: 'about', component: AboutComponent},
+    {path: 'about', component: AboutComponent, children: [
+            {path: 'extra', component: AboutExtraComponent}
+        ]},
     {path: 'posts', component: PostsComponent},
-    {path: 'posts/:id', component: PostComponent}
+    {path: 'posts/:id', component: PostComponent},
+    {path: 'error', component: ErrorPageComponent}, // TK: this component handles showing the app error page
+    {path: '**', redirectTo: '/error'} // TK: path '**' must always be in the end. It handles all unregistered/unknown routes
 ];
 // TK: custom module
 @NgModule({
