da81426dd5c9088b5d4dd8d78f600afe44996476
factactive <60749898+factactive@users.noreply.github.com>
2020-10-19 14:19:50 -0400

    finished initial routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,16 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from './home/home.component';
 import {AboutComponent} from './about/about.component';
-import {PostComponent} from './post/post.component';
+import {PostsComponent} from './posts/posts.component';
 
 const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'about', component: AboutComponent},
-    {path: 'posts', component: PostComponent}
+    {path: 'posts', component: PostsComponent}
 ];
 // TK: custom module
 @NgModule({
-    imports: [RouterModule],
+    imports: [RouterModule.forRoot(routes)],
     exports: [RouterModule]
 })
 export class AppRoutingModule {
